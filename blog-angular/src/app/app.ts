13759323f171e69d45215b7e13955aa6e816d933
Rafael Gomes <rafarfl2010@gmail.com>
2026-06-22 19:00:53 -0300

    feat(component): adding Toolbar + Router-outlet

--- blog-angular/src/app/app.ts
+++ blog-angular/src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from './core/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterLink, RouterOutlet, MatToolbarModule, MatButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('blog-angular');
+export class AppComponent {
+  authService = inject(AuthService);
+
+  async logout() {
+    await this.authService.signOut();
+  }
 }
